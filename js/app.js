66be87d6ad4f0cba54d3a1a2b997bc1cc80fc1e0
Florian Sobiegalla <arno_niem@me.com>
2015-06-08 10:19:04 +0200

    even moar magix

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function scrollTo(div) {
+    $('html, body').animate({ scrollTop: $(div).offset().top }, 'slow');
+    return false;
+}
+
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
